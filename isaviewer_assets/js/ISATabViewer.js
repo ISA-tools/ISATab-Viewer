bdf92328ea12d440a0f45fb9c4b1679aeff1421a
Eamonn Maguire <eamonnmag@gmail.com>
2014-06-20 14:18:40 +0800

    Investigation rendering now complete. Now adding in the assay view.

--- isaviewer_assets/js/ISATabViewer.js
+++ isaviewer_assets/js/ISATabViewer.js
@@ -97,28 +97,42 @@ ISATabViewer.rendering = {
 
         $("#isa-breadcrumb-items").html('<li class="active">' + studies[0] + '</li>');
 
-        ISATabViewer.rendering.render_study(studies[0]);
-
         var source = $("#study-list-template").html();
         var template = Handlebars.compile(source);
         var html = template({"studies": studies});
         $("#study-list").append(html);
+
+        ISATabViewer.rendering.render_study(studies[0]);
     },
 
-    render_study: function (study_id) {
-        console.log(study_id);
+    set_active_list_item: function (study_id) {
+        $("#study-list").find("li").each(function () {
+            $(this).removeClass("active");
+        });
+
+        $("#list-" + study_id).addClass("active");
+    }, render_study: function (study_id) {
+
+        this.set_active_list_item(study_id);
+
+        $("#isa-breadcrumb-items").html('<li class="active">' + study_id + '</li>');
         var study = {};
         for (var study_index in ISATabViewer.investigation.STUDY) {
             var study_information = ISATabViewer.investigation.STUDY[study_index];
 
-
             if (study_information.STUDY["Study Identifier"][0].indexOf(study_id) != -1) {
-
-                console.log('Found study!!');
                 study.study_id = ISATabViewer.rendering.replace_str("\"", "", study_information.STUDY["Study Identifier"][0]);
-                study.study_title = ISATabViewer.rendering.replace_str("\"", "",study_information.STUDY["Study Title"][0]);
-                study.study_description = ISATabViewer.rendering.replace_str("\"", "",study_information.STUDY["Study Description"][0]);
-                console.log(study);
+                study.study_title = ISATabViewer.rendering.replace_str("\"", "", study_information.STUDY["Study Title"][0]);
+                study.study_description = ISATabViewer.rendering.replace_str("\"", "", study_information.STUDY["Study Description"][0]);
+
+
+                study.publications = ISATabViewer.rendering.generate_records(study_information, "STUDY PUBLICATIONS");
+                study.protocols = ISATabViewer.rendering.generate_records(study_information, "STUDY PROTOCOLS");
+                study.contacts = ISATabViewer.rendering.generate_records(study_information, "STUDY CONTACTS");
+                study.factors = ISATabViewer.rendering.generate_records(study_information, "STUDY FACTORS");
+                study.assays = ISATabViewer.rendering.generate_records(study_information, "STUDY ASSAYS");
+
+                ISATabViewer.rendering.postprocess_assay_records(study.assays);
             }
         }
 
@@ -127,6 +141,46 @@ ISATabViewer.rendering = {
         var html = template(study);
 
         $("#study-info").html(html);
+    },
+
+    /*
+     This function adds in information about which images to use for example to show a particular type of assay.
+     */
+    postprocess_assay_records: function (records) {
+        for (var assay_index in records) {
+            var assay = records[assay_index];
+
+            var measurement_type = assay["Study Assay Measurement Type"];
+
+            console.log(measurement_type);
+
+            assay.icon = measurement_type.indexOf("metabolite") >= 0 ? "assay-icon-metabolomics"
+                : measurement_type.indexOf("prote") >= 0 ? "assay-icon-proteomics"
+                : measurement_type.indexOf("transcript") >= 0 ? "assay-icon-transcriptomics"
+                : measurement_type.indexOf("chemistry") >= 0 ? "assay-icon-chemistry"
+                : measurement_type.indexOf("genom") >= 0 ? "assay-icon-genomics"
+                : "";
+
+            console.log(assay.icon);
+        }
+    },
+
+    generate_records: function (study_information, field_name) {
+        var result = [];
+        for (var field in study_information[field_name]) {
+
+            var records = study_information[field_name][field];
+            for (var i = 0; i < records.length; i++) {
+                if (result[i] == undefined) {
+                    result[i] = {}
+                }
+
+
+                result[i][field] = ISATabViewer.rendering.replace_str("\"", "", records[i]);
+            }
+        }
+
+        return result;
     },
 
 
